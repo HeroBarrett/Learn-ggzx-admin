65558388b3f45ff4b6891cbde45b2baf6ec263a6
Dixon <510672083@qq.com>
2025-09-03 12:09:45 +0800

    feat:api接口统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <template>
   <div>
-    <h1>SVG测试</h1>
-    <SvgIcon name="aaa" color="green"></SvgIcon>
+    <h1>APP根组件</h1>
   </div>
-
 </template>
 
 <script lang="ts" setup>
-import request from './utils/request';
-  import { onMounted } from 'vue';
+  
 
-  onMounted(async () => {
-    const res = await request({
-      url: '/user/login',
-      method: 'post',
-      data: {
-        username: 'admin',
-        password: '111111'
-      }
-    })
-
-    console.log(res);
-  })
 </script>
 
-<style scoped lang="scss">
-  div {
-    h1 {
-      color: $base-color;
-    }
-  }
-</style>
+<style scoped></style>
