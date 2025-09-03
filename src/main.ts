4c265aec6cba6f0cd986c0646d8f294e41ce344e
Dixon <510672083@qq.com>
2025-09-03 13:24:43 +0800

    feat:登录页面-静态数据

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import App from './App.vue'
 // 引入 element-plus 插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 引入全局样式-清空默认样式等等
+import '@/components/styles/index.scss'
 // 配置element-plus国际化
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-// 引入模板的全局样式
-import '@/components/styles/index.scss'
 // SVG插件需要的配置代码
 // @ts-ignore
 import 'virtual:svg-icons-register'
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,66 @@
 <template>
-  <div>
-    <h1>我是一级路由登录</h1>
+  <div class="login_cotainer">
+    <el-row>
+      <el-col :span="12" :xs="0">占位的文字</el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User"  v-model="loginForm.username"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script lang="ts" setup>
-  
-
+  import { User, Lock } from '@element-plus/icons-vue'
+  import { reactive } from 'vue';
+  // 收集账号与密码数据
+  let loginForm = reactive({ username: '', password: '' })
 </script>
 
-<style scoped>
-  
+<style scoped lang="scss">
+  .login_cotainer {
+    width: 100%;
+    height: 100vh;
+    // background-color: red;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    background-size: cover;
+
+
+    .login_form {
+      position: relative;
+      width: 80%;
+      top: 30vh;
+      background: url("@/assets/images/login_form.png") no-repeat;
+      background-size: cover;
+      padding: 40px;
+
+      h1 {
+        color: white;
+        font-size: 40px;
+      }
+
+      h2 {
+        font-size: 20px;
+        color: white;
+        margin: 20px 0px;
+      }
+
+      .login_btn {
+        width: 100%;
+      }
+    }
+  }
+
 </style>
