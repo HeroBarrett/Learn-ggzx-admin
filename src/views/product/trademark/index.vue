9f83ed2ffee9ef87ff5d18086ffea85bc3a061a0
Dixon <510672083@qq.com>
2025-09-06 18:39:10 +0800

    feat:品牌管理-对话框静态搭建

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card>
     <!-- 卡片顶部按钮 -->
-    <el-button type="primary" size="default" icon="plus">添加品牌</el-button>
+    <el-button type="primary" size="default" icon="plus" @click="addTrademark">添加品牌</el-button>
     <!-- 表格组件：用于展示已有的品牌数据 -->
     <el-table style="margin: 10px 0;" border :data="trademarkArr">
       <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
@@ -17,7 +17,7 @@
       </el-table-column>
       <el-table-column label="品牌操作">
         <template #="{ row }">
-          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="primary" size="small" icon="Edit" @click="updateTrademark"></el-button>
           <el-button type="primary" size="small" icon="Delete"></el-button>
         </template>
       </el-table-column>
@@ -32,17 +32,35 @@
           background: 设置按钮背景颜色
           layout: 设置分页器六个字组件的布局
    -->
-    <el-pagination 
-      @current-change="changePageNo"
-      @size-change="sizeChange"
-      v-model:current-page="pageNo" 
-      v-model:page-size="limit" 
-      :page-sizes="[3, 5, 7, 9]"
-      :background="true" 
-      layout="prev, pager, next, jumper, ->, sizes, total" 
-      :total="total" />
+    <el-pagination @current-change="changePageNo" @size-change="sizeChange" v-model:current-page="pageNo"
+      v-model:page-size="limit" :page-sizes="[3, 5, 7, 9]" :background="true"
+      layout="prev, pager, next, jumper, ->, sizes, total" :total="total" />
   </el-card>
 
+
+  <!-- 对话框组件 -->
+  <!-- v-model:控制对话框显示与隐藏  title:设置对话框左上角的标题 -->
+  <el-dialog v-model="dialogVisible" title="添加品牌">
+    <el-form style="width: 80%;">
+      <el-form-item label="品牌名称" label-width="80px">
+        <el-input placeholder="请输入品牌名称"></el-input>
+      </el-form-item>
+      <el-form-item label="品牌logo" label-width="80px">
+        <el-upload class="avatar-uploader" action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+          :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon">
+            <Plus />
+          </el-icon>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+    <!-- 具名插槽:footer -->
+    <template #footer>
+      <el-button type="primary" size="default" @click="cancel">取消</el-button>
+      <el-button type="primary" size="default" @click="confirm">确定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
@@ -58,6 +76,8 @@
   let total = ref<number>(0)
   // 存储已有品牌的数据
   let trademarkArr = ref<Records>([])
+  // 对话框的显示与隐藏
+  let dialogVisible = ref<boolean>(false)
   // 获取已有品牌的接口
   const getHasTrademark = async () => {
     let result = await reqHasTrademark(pageNo.value, limit.value)
@@ -86,6 +106,55 @@
     getHasTrademark()
   }
 
+  // 添加品牌按钮的回调
+  const addTrademark = () => {
+    dialogVisible.value = true
+  }
+
+  // 修改品牌按钮的回调
+  const updateTrademark = () => {
+    dialogVisible.value = true
+  }
+
+  // 对话框底部取消按钮
+  const cancel = () => {
+    // 对话框隐藏
+    dialogVisible.value = false
+  }
+
+  const confirm = () => {
+    // 对话框隐藏
+    dialogVisible.value = false
+  }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
