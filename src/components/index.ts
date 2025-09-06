b5fbd9d8f7f68958d212215142e2dff0d472db31
Dixon <510672083@qq.com>
2025-09-06 22:48:32 +0800

    feat:属性管理-静态结构

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import SvgIcon from './SvgIcon/index.vue'
+import Category from './Category/index.vue'
 // 引入element-plus提供全部图标组件
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-const allGloablComponent: any = { SvgIcon }
+const allGloablComponent: any = { SvgIcon, Category }
 // 对外暴露插件对象
 export default {
   // 务必叫做install方法
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <h1>属性管理</h1>
+    <Category></Category>
+
+    <el-card style="margin: 10px 0">
+      <el-button type="primary" size="default" icon="Plus">添加属性</el-button>
+      <el-table border style="margin: 10px 0;">
+        <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+        <el-table-column label="属性名称" width="120px"></el-table-column>
+        <el-table-column label="属性值名称"></el-table-column>
+        <el-table-column label="操作" width="120px"></el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script lang="ts" setup>
-  
+
 
 </script>
 
-<style scoped lang="scss">
-  
-</style>
+<style scoped lang="scss"></style>
